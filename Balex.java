b5e44ca7c4fb86087218785d4ffbcd7ec3e3d40f
hamt44 <terwaneh@gmail.com>
2024-06-07 13:37:29 +0300

    Add files via upload

---
+++ Balex.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package oopexam.balex;
+
+/**
+ *
+ * @author YSJ COMPUTERS
+ */
+public class Balex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
